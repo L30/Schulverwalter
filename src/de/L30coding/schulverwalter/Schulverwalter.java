9d73db10040946b9fc5b3d75f3a3c9804059d536
Leo <l30coding@t-online.de>
2012-07-04 13:45:14 +0200

    First Commit of the new Project!
    Signed-off-by: Leo <l30coding@t-online.de>

---
+++ src/de/L30coding/schulverwalter/Schulverwalter.java
@@ -0,0 +1,13 @@
+package de.L30coding.schulverwalter;
+
+public class Schulverwalter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
